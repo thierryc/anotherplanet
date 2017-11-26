47708fbc11238f5dab395357b1f200e6d6b66d98
Thierry Charbonnel <thierryc@users.noreply.github.com>
2017-11-26 12:03:51 -0500

    update meta viewport

--- components/layout.js
+++ components/layout.js
@@ -19,8 +19,8 @@ export default class Layout extends Component {
       <div>
         <Head>
           <meta charSet='utf-8' />
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-          <link rel="shortcut icon" type='image/x-icon' href="/static/favicon.ico"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="shortcut icon" type='image/x-icon' href="/static/favicon.ico" />
           {/* Chrome, Firefox OS and Opera */}
           <meta name="theme-color" content="#282d47" />
         </Head>
